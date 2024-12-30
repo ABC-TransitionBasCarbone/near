bdbd445bdb00a9d51e29777b449383008513bcc3
Julien Parmentier <julien.parmentier@pathtech.fr>
2024-12-30 09:49:33 +0100

    chore: add vscode reco, config, load prettier conf in eslint

--- app-near/.eslintrc.cjs
+++ app-near/.eslintrc.cjs
@@ -5,14 +5,17 @@ const config = {
     "project": true
   },
   "plugins": [
-    "@typescript-eslint"
+    "@typescript-eslint",
+    "prettier"
   ],
   "extends": [
     "next/core-web-vitals",
     "plugin:@typescript-eslint/recommended-type-checked",
-    "plugin:@typescript-eslint/stylistic-type-checked"
+    "plugin:@typescript-eslint/stylistic-type-checked",
+    'prettier'
   ],
   "rules": {
+     "prettier/prettier": "error",
     "@typescript-eslint/array-type": "off",
     "@typescript-eslint/consistent-type-definitions": "off",
     "@typescript-eslint/consistent-type-imports": [
