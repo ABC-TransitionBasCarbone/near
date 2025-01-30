dad4391d1b5fe0677ff3703fd2ac5bb8c1b86e91
Jean-Bernard Peridy <jean-bernard.peridy@pathtech.fr>
2025-01-30 10:33:15 +0100

    feat/near-23 add security to not access other neighborood

--- app-near/src/server/api/routers/neighborhoods.ts
+++ app-near/src/server/api/routers/neighborhoods.ts
@@ -1,9 +1,13 @@
 import { z } from "zod";
 import { createTRPCRouter, protectedProcedure } from "../trpc";
 import { getOneNeighborood } from "~/server/neighboroods/get";
+import { TRPCError } from "@trpc/server";
 
 export const neighboroodsRouter = createTRPCRouter({
-  getOne: protectedProcedure.input(z.number()).query(({ input: id }) => {
+  getOne: protectedProcedure.input(z.number()).query(({ ctx, input: id }) => {
+    if (id !== ctx.session.user.surveyId) {
+      throw new TRPCError({ code: "FORBIDDEN" });
+    }
     return getOneNeighborood(id);
   }),
 });
