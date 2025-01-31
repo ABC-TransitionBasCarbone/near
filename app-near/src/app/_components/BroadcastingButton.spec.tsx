dfe92d0ef59e03b10a83ab66ea53aa0403270c47
Julien Parmentier <julien.parmentier@pathtech.fr>
2025-01-31 16:17:28 +0100

    feat(app): add basic fe test, update dep

---
+++ app-near/src/app/_components/BroadcastingButton.spec.tsx
@@ -0,0 +1,37 @@
+import { render } from "@testing-library/react";
+import BroadcastingButton from "./BroadcastingButton";
+import { type BroadcastType, type SurveyType } from "./BroadcastingPage";
+
+jest.mock("../../env", () => ({
+  env: {
+    NEXT_PUBLIC_TYPEFORM_SU_LINK: "https://example.com",
+  },
+}));
+
+Object.assign(navigator, {
+  clipboard: {
+    writeText: jest.fn(),
+  },
+});
+
+describe("BroadcastingButton", () => {
+  const surveyType: SurveyType = "su";
+  const broadcastType: BroadcastType = "mail_campaign";
+
+  it("renders correctly with given props", () => {
+    const { getByText } = render(
+      <BroadcastingButton
+        surveyType={surveyType}
+        broadcastType={broadcastType}
+      />,
+    );
+
+    expect(getByText("Enquête par email ou message")).toBeInTheDocument();
+    expect(
+      getByText(
+        "Générer un lien unique à intégrer dans vos emails ou vos messages",
+      ),
+    ).toBeInTheDocument();
+    expect(getByText("Générer un lien email")).toBeInTheDocument();
+  });
+});
