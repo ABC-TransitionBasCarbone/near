b687c113bd43afea7d3631967491252be01a6025
Julien Parmentier <julien.parmentier@pathtech.fr>
2025-01-31 10:16:18 +0100

    style: adjust max width

--- app-near/src/app/_components/_ui/SimpleBanner.tsx
+++ app-near/src/app/_components/_ui/SimpleBanner.tsx
@@ -6,7 +6,7 @@ interface SimpleBannerProps {
 const SimpleBanner: React.FC<SimpleBannerProps> = ({ children }) => {
   return (
     <div className="bg-grayExtraLight py-8">
-      <div className="mx-auto max-w-[850px]">{children}</div>
+      <div className="mx-auto max-w-5xl">{children}</div>
     </div>
   );
 };
