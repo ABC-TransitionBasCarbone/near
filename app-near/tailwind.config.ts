4bdb1800518b492b06d1767a635f0f0c4fed7a0f
jbperidypathtech <100352902+jbperidypathtech@users.noreply.github.com>
2025-01-31 11:36:56 +0100

    Feat/near 19 footer (#16)
    * feat/near-19 add footer
    
    * feat/near-19 after review
    
    * Feat/near 18 navbar (#15)
    
    * feat/near-23 create component iframe
    
    * feat/NEAR-23 add backend
    
    * feat/near-23 add security to not access other neighborood
    
    * feat/near-23 renaming
    
    * feat/near-18 create progress bar
    
    * feat/near-18 remove maxWidth
    
    * feat/near-18 remove not used props
    
    * feat/near-18 add spaces

--- app-near/tailwind.config.ts
+++ app-near/tailwind.config.ts
@@ -2,16 +2,17 @@ import { type Config } from "tailwindcss";
 import { fontFamily } from "tailwindcss/defaultTheme";
 
 export const colors = {
-  success: '#68BEAB',
-  error: '#D6296E',
-  info: '#00B8D9',
-  black: '#101010',
-  brownLight: '#FFECC7',
-  blue: '#0664F0',
-  blueLight: '#29ACD6',
-  white: '#FFFFFF',
-  grayLight: '#BFBFBF',
-  grayExtraLight: '#F6F6F6',
+  success: "#68BEAB",
+  error: "#D6296E",
+  info: "#00B8D9",
+  black: "#101010",
+  brownLight: "#FFECC7",
+  blue: "#0664F0",
+  blueLight: "#29ACD6",
+  white: "#FFFFFF",
+  grayLight: "#BFBFBF",
+  grayExtraLight: "#F6F6F6",
+  yellowLight: "#FFF2D4",
 };
 
 export default {
