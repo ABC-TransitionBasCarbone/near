7702492b0c2651fd03fb0ba13c7e933c1e3bf783
Jean-Bernard Peridy <jean-bernard.peridy@pathtech.fr>
2025-01-31 09:07:27 +0100

    feat/near-19 after review

--- app-near/src/app/_components/_ui/LinkAsButton.tsx
+++ app-near/src/app/_components/_ui/LinkAsButton.tsx
@@ -64,7 +64,7 @@ const LinkAsButton = ({
 
   return (
     <Link
-      className={`flex ${border ? "border" : ""} gap-3 ${iconRight ? "flex-row-reverse justify-between" : ""} ${colorVariants[style][color]} items-center px-5 py-2 font-sans font-bold ${underline ? "underline hover:no-underline" : "no-underline hover:underline"} ${customStyle} ${textColor ? textColorVariant[textColor] : ""} ${rounded ? "rounded-md" : ""} `}
+      className={`flex ${border ? "border" : ""} justify-center gap-3 ${iconRight ? "flex-row-reverse" : ""} ${colorVariants[style][color]} items-center px-5 py-2 font-sans font-bold ${underline ? "underline hover:no-underline" : "no-underline hover:underline"} ${customStyle} ${textColor ? textColorVariant[textColor] : ""} ${rounded ? "rounded-md" : ""} `}
       href={href}
       aria-label={ariaLabel}
       target={openNewTab ? "_blank" : "_self"}
