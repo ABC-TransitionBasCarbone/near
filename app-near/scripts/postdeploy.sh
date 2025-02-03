216df949325d4af7c7b51836e2c6a07242e2e204
Julien Parmentier <julien.parmentier@pathtech.fr>
2025-02-03 14:29:39 +0100

    fix(scalingo): check if insee data are loaded already

--- app-near/scripts/postdeploy.sh
+++ app-near/scripts/postdeploy.sh
@@ -2,7 +2,10 @@
 
 npm run prisma migrate deploy
 
-if [[ $IS_REVIEW_APP = "true" ]]; then
+ROW_COUNT=$(psql ${SCALINGO_POSTGRESQL_URL} -t -c "SELECT count(*) FROM public.insee_iris_2021;")
+
+if [[ $ROW_COUNT -eq 0 ]]; then
+    echo "Chargement des données INSEE 2021"
     curl -L -O https://www.insee.fr/fr/statistiques/fichier/8268806/base-ic-evol-struct-pop-2021_csv.zip
 
     unzip base-ic-evol-struct-pop-2021_csv.zip
@@ -12,6 +15,8 @@ if [[ $IS_REVIEW_APP = "true" ]]; then
     rm base-ic-evol-struct-pop-2021_csv.zip
 
     npm run load:insee -- "./scripts/base-ic-evol-struct-pop-2021.csv" ${SCALINGO_POSTGRESQL_URL}
+else 
+   echo "Les données INSEE 2021 sont déjà chargée"
 fi
 
 npm run load:surveys
