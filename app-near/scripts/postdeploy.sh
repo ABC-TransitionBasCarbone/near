c14425a73ae57f01a6555014b5b1e80806dc815d
Julien Parmentier <julien.parmentier@pathtech.fr>
2025-02-03 10:30:24 +0100

    feat(scalingo): split download, unsip task

--- app-near/scripts/postdeploy.sh
+++ app-near/scripts/postdeploy.sh
@@ -2,7 +2,13 @@
 
 npm run prisma migrate deploy
 
-curl -L -O https://www.insee.fr/fr/statistiques/fichier/8268806/base-ic-evol-struct-pop-2021_csv.zip && unzip base-ic-evol-struct-pop-2021_csv.zip && mv base-ic-evol-struct-pop-2021.CSV ./scripts/base-ic-evol-struct-pop-2021.csv && rm base-ic-evol-struct-pop-2021_csv.zip
+curl -L -O https://www.insee.fr/fr/statistiques/fichier/8268806/base-ic-evol-struct-pop-2021_csv.zip
+
+unzip base-ic-evol-struct-pop-2021_csv.zip
+
+mv base-ic-evol-struct-pop-2021.CSV ./scripts/base-ic-evol-struct-pop-2021.csv
+
+rm base-ic-evol-struct-pop-2021_csv.zip
 
 npm run load:insee -- "./base-ic-evol-struct-pop-2021.csv" ${SCALINGO_POSTGRESQL_URL}
 
