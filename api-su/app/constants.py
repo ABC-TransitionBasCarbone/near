52334d4eefb33cd8bed6a7015cdcabab726a4e07
Sandrine Ricardo <sandrine.ricardo@gmail.com>
2025-02-06 16:18:18 +0100

    chore(constants): add comment to help edition

--- api-su/app/constants.py
+++ api-su/app/constants.py
@@ -1,11 +1,16 @@
-CLUSTER_MIN_SIZE = 0.11
-CLUSTER_MAX_SIZE = 0.4
-CLUSTER_NB_MIN = 3
+CLUSTER_MIN_SIZE = 0.11 # Pourcentage minimal d'une sphère d'usage
+CLUSTER_MAX_SIZE = 0.4  # Pourcentage maximal d'une sphère d'usage
+CLUSTER_NB_MIN = 3      # Nombre minimal de sphères d'usage
 
-# Co2 equivalent if answer is [the first, the second, the third]
-RATIO_FOOD = [200, 1000, 2000]
-RATIO_MOBILITY = [0, 300, 2300]
-RATIO_DIGITAL = [90, 180, 360]
-RATIO_PURCHASE = [400, 800, 1600]
-RATIO_PLANE = [0, 800, 2000]
-RATIO_HOME = [200, 1100, 1600]
+# Ratio d'équivalent CO2 pour les réponses au questionnaire SU 
+# Pour chaque question, (ex: Combien de fois prenez-vous l'avion par an ?)
+# le tableau [X, Y, Z] correspond au ratio pour :
+# X = la première réponse (ex: 0)
+# Y = la deuxième réponse (ex: 1 à 3)
+# Z = la troisième réponse (ex: Plus de 3)
+RATIO_FOOD = [200, 1000, 2000]      # Question concernant la nourriture
+RATIO_MOBILITY = [0, 300, 2300]     # Question concernant la mobilité
+RATIO_DIGITAL = [90, 180, 360]      # Question concernant le numérique
+RATIO_PURCHASE = [400, 800, 1600]   # Question concernant les achats
+RATIO_PLANE = [0, 800, 2000]        # Question concernant l'avion
+RATIO_HOME = [200, 1100, 1600]      # Question concernant le chauffage
