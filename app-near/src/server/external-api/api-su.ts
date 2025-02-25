62eba69bddda61c08159d585aa17f3ad73dbcd1b
alleytuna <anais.altun@gmail.com>
2025-02-25 12:01:25 +0100

    chore(error): upgrade error handling

--- app-near/src/server/external-api/api-su.ts
+++ app-near/src/server/external-api/api-su.ts
@@ -5,6 +5,7 @@ import { env } from "~/env";
 import { type SuAnswerData, type SuComputationData } from "~/types/SuDetection";
 import { db } from "../db";
 import { convert } from "./convert";
+import { TRPCError } from "@trpc/server";
 
 export const buildSuComputationRequest = async (
   surveyId: number,
@@ -35,13 +36,16 @@ export const computeSus = async (
     // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
     const snakecaseData = await response.json();
 
-    if (response.status !== 200) {
+    if (!response.ok) {
       console.error(
         "Error when calling API SU compute:",
         response.status,
         snakecaseData,
       );
-      throw new Error(`HTTP Status=${response.status}`);
+      throw new TRPCError({
+        code: "BAD_REQUEST",
+        message: `API Error: HTTP Status=${response.status}}`,
+      });
     }
 
     const data = camelcaseKeys(snakecaseData, {
@@ -51,6 +55,15 @@ export const computeSus = async (
     return data;
   } catch (error) {
     console.error("Error calling API SU compute:", error);
-    throw error;
+    if (error instanceof TypeError) {
+      throw new TRPCError({
+        code: "INTERNAL_SERVER_ERROR",
+        message: "Network error occurred while calling the API.",
+      });
+    }
+    throw new TRPCError({
+      code: "INTERNAL_SERVER_ERROR",
+      message: "An unexpected error occurred.",
+    });
   }
 };
