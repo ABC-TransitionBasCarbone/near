0b174f7cca72b0a55e15969da4e33c5b081c0827
Julien Parmentier <julien.parmentier@pathtech.fr>
2025-02-26 13:31:20 +0100

    fix(webhook): split into another file for su form, process pr comment

--- app-near/src/server/typeform/handleAnswer.ts
+++ app-near/src/server/typeform/handleAnswer.ts
@@ -1,28 +1,18 @@
-import {
-  AgeCategory,
-  type SuAnswer,
-  type Survey,
-  SurveyPhase,
-} from "@prisma/client";
 import { type NextRequest, NextResponse } from "next/server";
 import { z } from "zod";
 import { env } from "~/env";
-import { type ConvertedSuAnswer } from "~/types/SuAnswer";
 import {
   type TypeformWebhookPayload,
   TypeformWebhookSchema,
 } from "~/types/typeform";
-import { createSu } from "../su/answers/create";
-import { getOneSurveyByName } from "../surveys/get";
 import { getReferencesMapping } from "../surveys/references";
 import { convertFormToAnswer } from "./convert";
-import { typeformSchemaMapper } from "./schema";
+import { handleSuForm } from "./handlers/handleSuForm";
 import { verifySignature } from "./signature";
 
 export const handleAnswer = async (req: NextRequest): Promise<NextResponse> => {
   let formId: string | undefined = undefined;
   try {
-    // parse body
     const body = await req.text();
     const parsedBody: TypeformWebhookPayload = TypeformWebhookSchema.parse(
       JSON.parse(body),
@@ -30,22 +20,18 @@ export const handleAnswer = async (req: NextRequest): Promise<NextResponse> => {
     formId = parsedBody.form_response.form_id;
     console.debug("[whebhook typeform]", formId, body);
 
-    // check typeform signature header
     if (!isValidSignature(req, body)) {
       return unauthorizedResponse(formId);
     }
 
-    // get reference mapping
     const referencesMapping = getReferencesMapping(formId);
     if (!referencesMapping) {
       return referenceMappingNotFoundResponse(formId);
     }
 
-    // convert
     const answers = convertFormToAnswer(parsedBody, referencesMapping);
     console.debug("[whebhook typeform]", formId, JSON.stringify(answers));
 
-    // switch case mapping depending on forms
     if (formId === env.SU_FORM_ID) {
       return await handleSuForm(parsedBody, answers, formId);
     }
@@ -72,63 +58,10 @@ export const handleAnswer = async (req: NextRequest): Promise<NextResponse> => {
   }
 };
 
-const handleSuForm = async (
-  parsedBody: TypeformWebhookPayload,
-  answers: Record<string, string | boolean>,
-  formId: string,
-): Promise<
-  NextResponse<{ message: string }> | NextResponse<{ error: string }>
-> => {
-  // should be part of neighborhood
-  if (isPartOfNeighborhood(answers)) {
-    return okResponse("user should live in neighborhood");
-  }
-
-  // should be under 15
-  if (isUnder15(answers)) {
-    return okResponse("user should be under 15");
-  }
-
-  // could throw zod exception from zod parsing
-  const parsedAnswer = typeformSchemaMapper[formId]?.parse(
-    answers,
-  ) as ConvertedSuAnswer;
-
-  // remove isNeighborhoodResident property
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const { isNeighborhoodResident, ...createQuery } = parsedAnswer;
-
-  // no survey name in hidden fields
-  const surveyName = parsedBody.form_response.hidden?.neighborhood;
-  if (!surveyName) {
-    return noSurveyNameProvidedResponse(formId);
-  }
-
-  // continue only if we are in phase 2
-  const survey = await getOneSurveyByName(surveyName);
-  if (!survey) {
-    return noSurveyFoundResponse(surveyName);
-  }
-
-  if (isNotInPhaseTwo(survey)) {
-    return notInPhaseSuSurveyResponse(surveyName);
-  }
-
-  // create su
-  const broadcastChannel = parsedBody.form_response.hidden?.broadcast_channel;
-
-  await createSu({ ...createQuery, broadcastChannel } as SuAnswer, surveyName);
-
-  return NextResponse.json({ message: "created" }, { status: 201 });
-};
-
 const isValidSignature = (req: NextRequest, body: string): boolean => {
   const signature = req.headers.get("Typeform-Signature");
   return verifySignature(signature, body);
 };
-
-const okResponse = (message: string): NextResponse<{ message: string }> =>
-  NextResponse.json({ message }, { status: 200 });
 
 const unknwonFormIdResponse = (formId?: string): NextResponse =>
   NextResponse.json(
@@ -142,23 +75,6 @@ const unauthorizedResponse = (formId?: string): NextResponse =>
     { status: 401 },
   );
 
-const noSurveyFoundResponse = (
-  surveyName?: string,
-): NextResponse<{ error: string }> => {
-  console.error("[whebhook typeform]", surveyName, "Survey not found");
-  return NextResponse.json(
-    { error: `Survey name ${surveyName} not found` },
-    { status: 404 },
-  );
-};
-
-const noSurveyNameProvidedResponse = (
-  formId?: string,
-): NextResponse<{ error: string }> => {
-  console.error("[whebhook typeform]", formId, "Survey name not found");
-  return NextResponse.json({ error: "Survey name not found" }, { status: 400 });
-};
-
 const referenceMappingNotFoundResponse = (formId?: string): NextResponse => {
   console.error(
     "[whebhook typeform]",
@@ -169,34 +85,4 @@ const referenceMappingNotFoundResponse = (formId?: string): NextResponse => {
     { error: "References mapping not found" },
     { status: 400 },
   );
-};
-
-const notInPhaseSuSurveyResponse = (
-  surveyName: string,
-): NextResponse<{ error: string }> => {
-  console.error(
-    "[whebhook typeform]",
-    surveyName,
-    `step ${SurveyPhase.STEP_2_SU_SURVERY} is over for ${surveyName}`,
-  );
-  return NextResponse.json(
-    {
-      error: `step ${SurveyPhase.STEP_2_SU_SURVERY} is over for ${surveyName}`,
-    },
-    { status: 200 },
-  );
-};
-
-const isUnder15 = (answers: Record<string, string | boolean>) => {
-  return !Object.values(AgeCategory).includes(
-    answers.ageCategory as AgeCategory,
-  );
-};
-
-const isPartOfNeighborhood = (answers: Record<string, string | boolean>) => {
-  return answers.isNeighborhoodResident !== true;
-};
-
-const isNotInPhaseTwo = (survey: Survey) => {
-  return survey.phase !== SurveyPhase.STEP_2_SU_SURVERY;
 };
