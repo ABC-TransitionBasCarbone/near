af6e5f6eebe97a0a991404956f91d472d3945ca7
Jean-Bernard Peridy <jean-bernard.peridy@pathtech.fr>
2025-03-03 15:36:58 +0100

    feat/NEAR-53 remove comment

--- app-near/src/server/external-api/api-su.ts
+++ app-near/src/server/external-api/api-su.ts
@@ -70,72 +70,72 @@ const computeSus = async (
   }
 };
 
-// ? those functions are not used
+const buildSuAssignmentRequest = async (
+  surveyId: number,
+  suAnswer: SuAnswer, // TODO: update with correct type
+): Promise<SuAssignementRequest> => {
+  const suDatas: SuData[] = await db.suData.findMany({
+    where: { surveyId },
+  });
+  return {
+    sus: suDatas.map((suData) => {
+      return { su: suData.su, barycenter: suData.barycenter as number[] };
+    }),
+    userData: convertToSuAnswerData(suAnswer),
+  };
+};
 
-// const buildSuAssignmentRequest = async (
-//   surveyId: number,
-//   suAnswer: SuAnswer, // TODO: update with correct type
-// ): Promise<SuAssignementRequest> => {
-//   const suDatas: SuData[] = await db.suData.findMany({
-//     where: { surveyId },
-//   });
-//   return {
-//     sus: suDatas.map((suData) => {
-//       return { su: suData.su, barycenter: suData.barycenter as number[] };
-//     }),
-//     userData: convertToSuAnswerData(suAnswer),
-//   };
-// };
+const assignSu = async (
+  payload: SuAssignementRequest,
+): Promise<AnswerAttributedSu> => {
+  try {
+    const snakeCaseAssignRequest = snakecaseKeys(
+      payload as unknown as Record<string, unknown>,
+    );
 
-// const assignSu = async (
-//   payload: SuAssignementRequest,
-// ): Promise<AnswerAttributedSu> => {
-//   try {
-//     const snakeCaseAssignRequest = snakecaseKeys(
-//       payload as unknown as Record<string, unknown>,
-//     );
+    const response = await fetch(`${env.API_SU_URL}/api-su/assign`, {
+      method: "POST",
+      headers: {
+        "X-API-KEY": `${env.API_SU_KEY}`,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(snakeCaseAssignRequest),
+    });
 
-//     const response = await fetch(`${env.API_SU_URL}/api-su/assign`, {
-//       method: "POST",
-//       headers: {
-//         "X-API-KEY": `${env.API_SU_KEY}`,
-//         "Content-Type": "application/json",
-//       },
-//       body: JSON.stringify(snakeCaseAssignRequest),
-//     });
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+    const snakeCaseAssignResponse = await response.json();
 
-//     // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-//     const snakeCaseAssignResponse = await response.json();
+    if (!response.ok) {
+      throw new Error(
+        `Status=${response.status} Content=${snakeCaseAssignResponse}`,
+      );
+    }
 
-//     if (!response.ok) {
-//       throw new Error(
-//         `Status=${response.status} Content=${snakeCaseAssignResponse}`,
-//       );
-//     }
+    const camelCaseAssignResponse = camelcaseKeys(snakeCaseAssignResponse, {
+      deep: true,
+    }) as AnswerAttributedSu;
 
-//     const camelCaseAssignResponse = camelcaseKeys(snakeCaseAssignResponse, {
-//       deep: true,
-//     }) as AnswerAttributedSu;
-
-//     return camelCaseAssignResponse;
-//   } catch (error) {
-//     console.error("Error calling API SU assign:", error);
-//     if (error instanceof TypeError) {
-//       throw new TRPCError({
-//         code: "INTERNAL_SERVER_ERROR",
-//         message: "Network error occurred while calling the API SU assign.",
-//       });
-//     }
-//     throw new TRPCError({
-//       code: "INTERNAL_SERVER_ERROR",
-//       message: ErrorCode.UNEXPECTED_ERROR,
-//     });
-//   }
-// };
+    return camelCaseAssignResponse;
+  } catch (error) {
+    console.error("Error calling API SU assign:", error);
+    if (error instanceof TypeError) {
+      throw new TRPCError({
+        code: "INTERNAL_SERVER_ERROR",
+        message: "Network error occurred while calling the API SU assign.",
+      });
+    }
+    throw new TRPCError({
+      code: "INTERNAL_SERVER_ERROR",
+      message: ErrorCode.UNEXPECTED_ERROR,
+    });
+  }
+};
 
 const apiSuService = {
   buildSuComputationRequest,
   computeSus,
+  buildSuAssignmentRequest,
+  assignSu,
 };
 
 export default apiSuService;
