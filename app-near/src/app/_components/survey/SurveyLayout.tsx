fd5f9617d75f6771d7bdf77b32cfd1d7411bfdf7
JulienPathtech <100351051+JulienPathtech@users.noreply.github.com>
2025-03-18 12:13:45 +0100

    fix(webapp): display button only on phase 1 (#69)
    * fix(webapp): display button only on phase 1
    
    * fix/NEAR-70 change action display
    
    ---------
    
    Co-authored-by: Jean-Bernard Peridy <jean-bernard.peridy@pathtech.fr>

--- app-near/src/app/_components/survey/SurveyLayout.tsx
+++ app-near/src/app/_components/survey/SurveyLayout.tsx
@@ -1,5 +1,8 @@
 import { type ReactNode } from "react";
 import SimpleBanner from "../_ui/SimpleBanner";
+import { api } from "~/trpc/react";
+import { useSession } from "next-auth/react";
+import { useSurveyStateContext } from "../_context/surveyStateContext";
 
 interface SurveyLayoutProps {
   children: ReactNode;
@@ -11,15 +14,23 @@ const SurveyLayout: React.FC<SurveyLayoutProps> = ({
   banner,
   actions,
 }) => {
+  const { data: session } = useSession();
+  const { data: survey } = api.surveys.getOne.useQuery(undefined, {
+    enabled: !!session?.user?.surveyId,
+  });
+  const { step } = useSurveyStateContext();
+
   return (
     <div className="flex flex-1 flex-col">
       <div className="flex-grow">
         <SimpleBanner>{banner}</SimpleBanner>
         <div>{children}</div>
       </div>
-      <div className="sticky bottom-0 flex w-full flex-col flex-wrap justify-center gap-4 bg-white p-4 py-8 shadow-[0_-4px_5px_-5px_gray] sm:flex-row">
-        {actions}
-      </div>
+      {step === survey?.phase && (
+        <div className="sticky bottom-0 flex w-full flex-col flex-wrap justify-center gap-4 bg-white p-4 py-8 shadow-[0_-4px_5px_-5px_gray] sm:flex-row">
+          {actions}
+        </div>
+      )}
     </div>
   );
 };
