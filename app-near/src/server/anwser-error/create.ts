1e30778aeabc9ad63a4aa6ad998563de6fae1042
Julien Parmentier <julien.parmentier@pathtech.fr>
2025-05-20 15:18:20 +0200

    fix(app): better ts handling

--- app-near/src/server/anwser-error/create.ts
+++ app-near/src/server/anwser-error/create.ts
@@ -1,16 +1,13 @@
 import { type AnswerType } from "@prisma/client";
 import { db } from "../db";
+import { type InputJsonValue } from "@prisma/client/runtime/library";
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const createAnswerError = async (rawPayload: any, type: AnswerType) => {
-  await db.rawAnswerError.create({
+  return await db.rawAnswerError.create({
     data: {
-      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-      rawPayload,
-      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+      rawPayload: rawPayload as InputJsonValue,
       answerType: type,
     },
   });
-
-  return;
 };
