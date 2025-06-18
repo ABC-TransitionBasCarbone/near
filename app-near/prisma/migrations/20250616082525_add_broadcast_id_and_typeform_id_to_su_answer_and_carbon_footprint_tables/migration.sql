2a306ae39a459585bf1e04ee4b370fcf51bb07f3
Jean-Bernard Peridy <jean-bernard.peridy@pathtech.fr>
2025-06-18 12:05:37 +0200

    fix/near-93 add default values

--- app-near/prisma/migrations/20250616082525_add_broadcast_id_and_typeform_id_to_su_answer_and_carbon_footprint_tables/migration.sql
+++ app-near/prisma/migrations/20250616082525_add_broadcast_id_and_typeform_id_to_su_answer_and_carbon_footprint_tables/migration.sql
@@ -1,12 +1,3 @@
-/*
-  Warnings:
-
-  - Added the required column `broadcast_id` to the `su_answer` table without a default vunknownalue. This is not possible if the table is not empty.
-  - Added the required column `typeform_id` to the `su_answer` table without a default value. This is not possible if the table is not empty.
-  - Added the required column `broadcast_id` to the `way_of_life_answer` table without a default value. This is not possible if the table is not empty.
-  - Added the required column `typeform_id` to the `way_of_life_answer` table without a default value. This is not possible if the table is not empty.
-
-*/
 -- AlterTable
 ALTER TABLE "su_answer" ADD COLUMN     "broadcast_id" TEXT NOT NULL DEFAULT 'unknown',
 ADD COLUMN     "typeform_id" TEXT NOT NULL DEFAULT 'unknown';
