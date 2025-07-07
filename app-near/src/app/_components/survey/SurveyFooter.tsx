7d9782b462a4a17dea93ad14e04d9170543068a1
alleytuna <anais.altun@gmail.com>
2025-07-07 15:48:21 +0200

    feat(footer): update

--- app-near/src/app/_components/survey/SurveyFooter.tsx
+++ app-near/src/app/_components/survey/SurveyFooter.tsx
@@ -1,25 +1,28 @@
 import Link from "next/link";
 
 const SurveyFooter: React.FC = () => {
+  const logoStyle = "h-[35px] p-2 md:h-[61px]";
+  const logosWrapper =
+    "my-auto flex flex-wrap justify-center gap-8 rounded-lg bg-white";
   return (
-    <div className="mb-8 flex flex-col gap-8">
-      <div className="flex flex-wrap justify-center gap-8 bg-grayExtraLight px-12 py-4 md:justify-between">
-        <div className="my-auto flex flex-wrap justify-center gap-8">
-          <img className="h-[61px]" src="/logos/abc.webp" alt="" />
-          <img className="h-[61px]" src="/logos/reseau-qet.webp" alt="" />
-          <img className="h-[61px]" src="/logos/ville-paris.webp" alt="" />
+    <div className="mt-5 flex min-h-[166px] flex-col gap-5 bg-[url(/images/pattern_vert.webp)]">
+      <div className="items-center justify-between px-10 pt-14 md:flex md:flex-wrap">
+        <div className={logosWrapper}>
+          <img className={logoStyle} src="/logos/abc.webp" alt="" />
+          <img className={logoStyle} src="/logos/reseau-qet.webp" alt="" />
+          <img className={logoStyle} src="/logos/ville-paris.webp" alt="" />
           <img
-            className="h-[61px]"
+            className={logoStyle}
             src="/logos/cooperative-carbone.webp"
             alt=""
           />
         </div>
-        <div className="my-auto flex flex-wrap justify-center gap-8">
-          <img className="h-[61px]" src="/logos/net-zero-cities.webp" alt="" />
-          <img className="h-[61px]" src="/logos/ue.webp" alt="" />
+        <div className={logosWrapper}>
+          <img className={logoStyle} src="/logos/net-zero-cities.webp" alt="" />
+          <img className={logoStyle} src="/logos/ue.webp" alt="" />
         </div>
       </div>
-      <div className="text-center">
+      <div className="text-center text-xs">
         © NEAR 2025 --{" "}
         <Link className="font-normal text-black" href="/mentions-legales">
           Mentions légales
