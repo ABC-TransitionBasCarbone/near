27f5c3e22a400beddce7cb883a26750b9ab0e8b3
Jean-Bernard Peridy <jean-bernard.peridy@pathtech.fr>
2025-10-21 17:00:21 +0200

    feat/near-111 use mutation to get iris

--- app-near/src/server/insee/get.ts
+++ app-near/src/server/insee/get.ts
@@ -4,5 +4,6 @@ export const getAllInseeIris = async (search: string) => {
   return db.inseeIris2021.findMany({
     where: { iris: { contains: search } },
     select: { iris: true },
+    take: 10,
   });
 };
