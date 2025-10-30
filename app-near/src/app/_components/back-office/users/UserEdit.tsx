f19162c22583862cf26cee4b9e7259be13b3ce04
Julien Parmentier <julien.parmentier@pathtech.fr>
2025-10-30 12:42:26 +0100

    feat(users): add password modal and redirect

--- app-near/src/app/_components/back-office/users/UserEdit.tsx
+++ app-near/src/app/_components/back-office/users/UserEdit.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useRouter } from "next/navigation";
+import { useState } from "react";
 import { FormProvider, type SubmitHandler, useForm } from "react-hook-form";
 import {
   userform,
@@ -13,10 +14,13 @@ import { getErrorValue } from "../../_services/error";
 import Button from "../../_ui/Button";
 import FormInput from "../../_ui/form/FormInput";
 import FormSelect from "../../_ui/form/FormSelect";
+import PasswordModal from "./PasswordModal";
 
 const UserEdit: React.FC = () => {
   const utils = api.useUtils();
   const surveyList = api.surveys.querySurveys.useQuery({ limit: 100 });
+  const [userPassword, setUserPassword] = useState<string | null>(null);
+  const [showModal, setShowModal] = useState<boolean>(false);
 
   const methods = useForm<UserForm>({
     resolver: zodResolver(userform),
@@ -31,9 +35,8 @@ const UserEdit: React.FC = () => {
   const createUserMutation = api.users.create.useMutation({
     onSuccess: async (data) => {
       await utils.users.queryUsers.invalidate();
-      console.log("passord", data?.password);
-      // TODO show password
-      // router.push("/back-office/utilisateurs");
+      setUserPassword(data?.password ?? null);
+      setShowModal(true);
     },
     onError: async (error) => {
       setNotification({
@@ -43,53 +46,68 @@ const UserEdit: React.FC = () => {
     },
   });
 
+  const onPasswordModalClose = () => {
+    setShowModal(false);
+    router.push("/back-office/utilisateurs");
+  };
+
   const onSubmit: SubmitHandler<UserForm> = async (values) => {
     await createUserMutation.mutateAsync(values);
   };
 
   return (
-    <div className="m-auto flex max-w-lg justify-center">
-      <FormProvider {...methods}>
-        <form
-          onSubmit={handleSubmit(onSubmit)}
-          className="flex w-full flex-col gap-3"
-        >
-          <h1>Nouvel utilisateur</h1>
-          <FormInput
-            id="email"
-            name="email"
-            label="Email de l'utilisateur"
-            type="email"
-            placeholder="Saisissez l'email de l'utilisateur"
-            required
-          />
-          <FormSelect
-            name="surveyId"
-            label="Quartier de l'utilisateur"
-            required
-            options={
-              surveyList.data?.items.map((survey) => ({
-                id: survey.id,
-                value: survey.name,
-              })) ?? []
-            }
-          ></FormSelect>
+    <>
+      {userPassword && (
+        <PasswordModal
+          showModal={showModal}
+          onPasswordModalClose={onPasswordModalClose}
+          password={userPassword}
+        />
+      )}
 
-          <div className="flex justify-center gap-6">
-            <Button
-              className="mt-6"
-              color="gray"
-              onClick={() => router.push("/back-office/utilisateurs")}
-            >
-              Annuler
-            </Button>
-            <Button type="submit" className="mt-6">
-              Ajouter
-            </Button>
-          </div>
-        </form>
-      </FormProvider>
-    </div>
+      <div className="m-auto flex max-w-lg justify-center">
+        <FormProvider {...methods}>
+          <form
+            onSubmit={handleSubmit(onSubmit)}
+            className="flex w-full flex-col gap-3"
+          >
+            <h1>Nouvel utilisateur</h1>
+            <FormInput
+              id="email"
+              name="email"
+              label="Email de l'utilisateur"
+              type="email"
+              placeholder="Saisissez l'email de l'utilisateur"
+              required
+            />
+            <FormSelect
+              name="surveyId"
+              label="Quartier de l'utilisateur"
+              required
+              options={
+                surveyList.data?.items.map((survey) => ({
+                  id: survey.id,
+                  value: survey.name,
+                })) ?? []
+              }
+            ></FormSelect>
+
+            <div className="flex justify-center gap-6">
+              <Button
+                className="mt-6"
+                color="gray"
+                onClick={() => router.push("/back-office/utilisateurs")}
+              >
+                Annuler
+              </Button>
+              <Button type="submit" className="mt-6">
+                Ajouter
+              </Button>
+            </div>
+          </form>
+        </FormProvider>
+      </div>
+    </>
   );
 };
 
