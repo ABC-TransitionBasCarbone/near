de4cdc704160ebf84e7e838180a58c651edd2e35
Jean-Bernard Peridy <jean-bernard.peridy@pathtech.fr>
2026-08-20 12:12:25 +0200

    feat/near-130 remove test preview page

--- app-near/src/app/dvpreview-tmp/page.tsx
+++
@@ -1,9 +0,0 @@
-import DatavizDashboard from "~/app/_components/dataviz/DatavizDashboard";
-
-export default function DvPreviewTmpPage() {
-  return (
-    <div style={{ height: "100vh" }}>
-      <DatavizDashboard />
-    </div>
-  );
-}
